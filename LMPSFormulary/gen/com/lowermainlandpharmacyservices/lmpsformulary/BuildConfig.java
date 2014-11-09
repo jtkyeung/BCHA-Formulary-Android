a1c29468ee7681d999c2fc2213d651ed425f77ef
Jessica Yeung <jessica.yeung@hotmail.com>
2014-11-09 14:04:55 -0800

    New Project

---
+++ LMPSFormulary/gen/com/lowermainlandpharmacyservices/lmpsformulary/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.lowermainlandpharmacyservices.lmpsformulary;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ LMPSFormulary/src/com/lowermainlandpharmacyservices/lmpsformulary/MainActivity.java
@@ -0,0 +1,77 @@
+package com.lowermainlandpharmacyservices.lmpsformulary;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+	
+	
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+	
+	
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+    /*public void displayResult(View view) throws Exception {
+        Intent searchResult = new Intent(this, DisplaySearchResult.class);
+        EditText editText = (EditText) findViewById(R.id.search_input);
+        String searchInput = editText.getText().toString();
+//Kelvin's recommendation---------------------------------------
+        CSVParser parser = new CSVParser();
+        DrugList supplyList = parser.getSupplyList();
+        NameList nameList = parser.getNameList();
+        if (supplyList.containsGenericName(searchInput)) {
+            searchResult.putExtra(EXTRA_INFO, supplyList.getDrug(searchInput));
+            startActivity(searchResult);
+        } else if (nameList.containsBrandName(searchInput)) {
+            searchResult.putExtra(EXTRA_INFO,
+                    supplyList.getDrug(nameList.getGenericName(searchInput)));
+            startActivity(searchResult);
+        } else {
+            Toast.makeText(this, searchInput + " is non-Forumulary or typo has been made",
+                            Toast.LENGTH_LONG).show();
+        }
+
+
+//    public Drug searchDrug(String input) throws Exception {
+//        CSVParser parser = new CSVParser();
+//        DrugList supplyList = parser.getSupplyList();
+//        NameList nameList = parser.getNameList();
+//        if (parser.inSystem(input)) {
+//            if (supplyList.containsGenericName(input)) {
+//                return supplyList.getDrug(input);
+//            } else if (nameList.containsBrandName(input)) {
+//                return supplyList.getDrug(nameList.getGenericName(input));
+//            }
+//        } else {
+//            return null;
+//        }
+//
+//    }
+    } */
+}
