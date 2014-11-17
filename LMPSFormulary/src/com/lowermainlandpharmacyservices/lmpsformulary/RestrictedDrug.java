c40c5fa89ae6c9b350536bb7c529e50dbaa0f75e
chan-kelv <chan.kelv@gmail.com>
2014-11-16 22:33:42 -0800

    Changed restricted criteria to allow for multiple rows of description.

--- LMPSFormulary/src/com/lowermainlandpharmacyservices/lmpsformulary/RestrictedDrug.java
+++ LMPSFormulary/src/com/lowermainlandpharmacyservices/lmpsformulary/RestrictedDrug.java
@@ -1,21 +1,18 @@
 package com.lowermainlandpharmacyservices.lmpsformulary;
 
 public class RestrictedDrug extends Drug {
-	private String criteria;
+	private StringBuffer criteria;
 
 	public RestrictedDrug(String genericName, String brandName, String criteria){
         super(genericName, brandName, "Restricted");
-//		super.genericName = genericName;
-//		super.brandName = brandName;
-//		super.status = "Restricted";
-		this.criteria = criteria;
+		this.criteria = new StringBuffer(criteria);
 	}
 
-	public String getCriteria(){
-		return criteria;
+	public String getCriteriaString(){
+		return criteria.toString();
 	}
 
-    public void setCriteria(String newCriteria){
-        this.criteria = newCriteria;
+    public void additionalCriteria(String extraCriteria){
+        this.criteria.append(extraCriteria);
     }
 }
