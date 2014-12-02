a29fde62a8a73ceebd5edb6f834101a3e74f62fd
chan-kelv <chan.kelv@gmail.com>
2014-12-02 11:44:08 -0800

    Fixed wifi similar to it was before the accidental revert

--- LMPSFormulary/src/com/lowermainlandpharmacyservices/lmpsformulary/DownloadTask.java
+++ LMPSFormulary/src/com/lowermainlandpharmacyservices/lmpsformulary/DownloadTask.java
@@ -1,25 +1,13 @@
 package com.lowermainlandpharmacyservices.lmpsformulary;
 
-import java.io.BufferedReader;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
-import org.apache.http.HttpResponse;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.protocol.BasicHttpContext;
-import org.apache.http.protocol.HttpContext;
-
 import android.content.Context;
 import android.os.AsyncTask;
-import android.os.Environment;
 import android.os.PowerManager;
 import android.widget.Toast;
 
