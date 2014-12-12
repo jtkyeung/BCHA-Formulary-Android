e294a1a2be3271d24e3adb890f45e06f98d112e9
chan-kelv <chan.kelv@gmail.com>
2014-12-11 16:15:36 -0800

    Changed check wifi to check network. Added network permission-manifest

--- LMPSFormulary/src/com/lowermainlandpharmacyservices/lmpsformulary/MainActivity.java
+++ LMPSFormulary/src/com/lowermainlandpharmacyservices/lmpsformulary/MainActivity.java
@@ -11,7 +11,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.AssetManager;
-import android.net.wifi.WifiManager;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -37,7 +38,7 @@ public class MainActivity extends Activity {
 
 	public AssetManager assetManager;
 	private Boolean toParse = true;
-	private boolean wifiIsOn;
+	private boolean isConnected;
 	private boolean updateNeeded = false;
 
 	@Override
@@ -78,10 +79,16 @@ public class MainActivity extends Activity {
 				updateNeeded = true;
 				System.out.println("We need an update!");
 				Toast.makeText(this, "File update in progress", Toast.LENGTH_LONG).show();
-				//wifi check
-				WifiManager wifi = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+//				//wifi check
+//				WifiManager wifi = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+				//network check
+				ConnectivityManager cm =
+				        (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+				 
+				NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+				isConnected = activeNetwork != null && activeNetwork.isConnectedOrConnecting();
 				//if wifi is on	
-				if(wifi.isWifiEnabled()){
+				if(isConnected){
 					// execute this when the downloader must be fired
 					final DownloadTask downloadFormulary = new DownloadTask(MainActivity.this, "formularyUpdated.csv");
 					downloadFormulary.execute("https://www.dropbox.com/sh/ctdjnxoemlx9hbr/AABotiW6CP_-JrGAh0mw1nkma/formulary.csv?dl=1").get();
@@ -89,7 +96,6 @@ public class MainActivity extends Activity {
 					downloadExcluded.execute("https://www.dropbox.com/sh/ctdjnxoemlx9hbr/AAAh2jkw2watr9KpopeH_JUsa/excluded.csv?dl=1").get();
 					final DownloadTask downloadRestricted = new DownloadTask(MainActivity.this, "restrictedUpdated.csv");
 					downloadRestricted.execute("https://www.dropbox.com/sh/ctdjnxoemlx9hbr/AACa_xqMx2PZWMoWKe5tJoRda/restricted.csv?dl=1").get();
-					wifiIsOn = true;
 
 					// We need an Editor object to make preference changes.
 					// All objects are from android.context.Context
@@ -100,7 +106,6 @@ public class MainActivity extends Activity {
 				else{ //if wifi is off
 					Toast.makeText(this, "A version update is available, please connect to wi-fi "
 							+ "and restart to app to update", Toast.LENGTH_LONG).show();
-					wifiIsOn= false;
 				}
 			}
 		}
