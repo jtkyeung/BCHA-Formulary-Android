f380d3ad60c4df1436e787f1495e30f8343db349
chan-kelv <chan.kelv@gmail.com>
2014-12-22 05:15:02 -0800

    Changed up extra criteria to remove encoding issues

--- LMPSFormulary/src/com/lowermainlandpharmacyservices/lmpsformulary/BrandExcludedDrug.java
+++ LMPSFormulary/src/com/lowermainlandpharmacyservices/lmpsformulary/BrandExcludedDrug.java
@@ -14,14 +14,40 @@ public class BrandExcludedDrug extends BrandDrug {
 
 	public void additionalCriteria(String extraCriteria){
 		StringBuilder extraAddition = new StringBuilder();
+		String punctutations = ".,':;<>/=()-";
 
-		//add bullet
-		if(!(extraCriteria.contains(":")|| extraCriteria.contains("OR")&&extraCriteria.length()==2)){
-			extraAddition.append("-   ");
+		//ignore numbers
+		if(Character.isDigit(extraCriteria.charAt(0))){
+			extraAddition.append(extraCriteria.charAt(0));
+			extraAddition.append(".  ");
 		}
-		
+		//add bullet
+		else if(!(extraCriteria.contains(":")|| extraCriteria.contains("OR")&&extraCriteria.length()<=3)){
+			extraAddition.append("    -   ");
+		}
+
+		//find the first letter
+		int j = 0;
+		boolean foundFirstLetter = false;
 		for (int i = 0; i < extraCriteria.length();i++){
-			extraAddition.append(extraCriteria.charAt(i));
+			if(!(foundFirstLetter)){
+				if(!(Character.isAlphabetic(extraCriteria.charAt(i)))){
+					j++;
+				}
+				else
+					foundFirstLetter = true;
+			}
+		}
+
+		//append all from first letter till end
+		char charToAdd;
+		for (int k=j; k < extraCriteria.length();k++){
+			charToAdd = extraCriteria.charAt(k);
+			if(Character.isLetterOrDigit(charToAdd)||Character.isWhitespace(charToAdd)||
+					Character.isSpaceChar(charToAdd)||punctutations.contains(String.valueOf(charToAdd)))
+				extraAddition.append(extraCriteria.charAt(k));
+			else
+				extraAddition.append("'");
 		}
 		this.criteria.insert(criteria.length(), ("\n\n"+extraAddition.toString()));	
 	}
--- LMPSFormulary/src/com/lowermainlandpharmacyservices/lmpsformulary/GenericRestrictedDrug.java
+++ LMPSFormulary/src/com/lowermainlandpharmacyservices/lmpsformulary/GenericRestrictedDrug.java
@@ -5,7 +5,7 @@ public class GenericRestrictedDrug extends GenericDrug {
 	public StringBuilder criteria;
 
 	public GenericRestrictedDrug(String genericName, String brandName, String criteria){
-        super(genericName, brandName, "Restricted");
+		super(genericName, brandName, "Restricted");
 		this.criteria = new StringBuilder(criteria);
 	}
 
@@ -13,17 +13,43 @@ public class GenericRestrictedDrug extends GenericDrug {
 		return criteria.toString();
 	}
 
-    public void additionalCriteria(String extraCriteria){
-    	StringBuilder extraAddition = new StringBuilder();
-    	
-    	//add bullet
-    	if(!(extraCriteria.contains(":")|| extraCriteria.contains("OR")&&extraCriteria.length()==2)){
-    		extraAddition.append("-   ");
-    	}
-    	
-    	for (int i = 0; i < extraCriteria.length();i++){
-    			extraAddition.append(extraCriteria.charAt(i));
-    	}
-    	this.criteria.insert(criteria.length(), ("\n\n"+extraAddition.toString()));	
-    }
+	public void additionalCriteria(String extraCriteria){
+		StringBuilder extraAddition = new StringBuilder();
+		String punctutations = ".,':;<>/=()-";
+
+		//ignore numbers
+		if(Character.isDigit(extraCriteria.charAt(0))){
+			extraAddition.append(extraCriteria.charAt(0));
+			extraAddition.append(".  ");
+		}
+		//add bullet
+		else if(!(extraCriteria.contains(":")|| extraCriteria.contains("OR")&&extraCriteria.length()<=3)){
+			extraAddition.append("    -   ");
+		}
+
+		//find the first letter
+		int j = 0;
+		boolean foundFirstLetter = false;
+		for (int i = 0; i < extraCriteria.length();i++){
+			if(!(foundFirstLetter)){
+				if(!(Character.isAlphabetic(extraCriteria.charAt(i)))){
+					j++;
+				}
+				else
+					foundFirstLetter = true;
+			}
+		}
+
+		//append all from first letter till end
+		char charToAdd;
+		for (int k=j; k < extraCriteria.length();k++){
+			charToAdd = extraCriteria.charAt(k);
+			if(Character.isLetterOrDigit(charToAdd)||Character.isWhitespace(charToAdd)||
+					Character.isSpaceChar(charToAdd)||punctutations.contains(String.valueOf(charToAdd)))
+				extraAddition.append(extraCriteria.charAt(k));
+			else
+				extraAddition.append("'");
+		}
+		this.criteria.insert(criteria.length(), ("\n\n"+extraAddition.toString()));	
+	}
 }
